df04e6a4e2bdd3c8fa3bba313adb37d04ef81add
kpestov <kpestov91@gmail.com>
2019-02-19 22:26:43 +0500

    added analytics

--- nasdaqengine/nasdaqstat/urls.py
+++ nasdaqengine/nasdaqstat/urls.py
@@ -4,5 +4,11 @@ from .views import *
 
 urlpatterns = [
     path('', TickerList.as_view(), name='ticker_list_url'),
-    path('<str:ticker>/', TickerHistorical.as_view(), name='ticker_historical_url')
+    path('<str:ticker>/', TickerHistorical.as_view(), name='ticker_historical_url'),
+    path('<str:ticker>/insider/', TickerInsiderTrades.as_view(), name='ticker_insider_trades_url'),
+    path('<str:ticker>/insider/<str:insider_name>/', InsiderOperations.as_view(), name='insider_operations_url'),
+    path('<str:ticker>/analytics', Analytics.as_view(), name='analytics_url'),
 ]
+
+
+# http://127.0.0.1:8000/goog/analytics?date_from=2019-02-15&date_to=2019-02-14
--- nasdaqengine/nasdaqstat/views.py
+++ nasdaqengine/nasdaqstat/views.py
@@ -18,3 +18,56 @@ class TickerHistorical(View):
 
         return render(request, 'nasdaqstat/historical.html', context={'ticker': ticker,
                                                                       'ticker_historical': ticker_historical})
+
+
+class TickerInsiderTrades(View):
+    def get(self, request, ticker):
+
+        ticker_obj = Company.objects.get(ticker=ticker)
+
+        ticker_insider_trades = InsiderTrades.objects.filter(ticker=ticker_obj)
+
+        return render(request, 'nasdaqstat/insider_trades.html', context={'ticker': ticker,
+                                                                          'ticker_insider_trades': ticker_insider_trades})
+
+
+class InsiderOperations(View):
+    def get(self, request, ticker, insider_name):
+        ticker_obj = Company.objects.get(ticker=ticker)
+        insider_obj = Insider.objects.get(name=insider_name)
+        insider_operations = InsiderTrades.objects.filter(ticker=ticker_obj).filter(insider=insider_obj.id)
+
+        context = {'ticker': ticker,
+                   'insider_operations': insider_operations,
+                   'insider_name': insider_name,
+                   }
+
+        return render(request, 'nasdaqstat/insider_operations.html', context)
+
+
+class Analytics(View):
+    def get(self, request, ticker):
+        analytics_query = request.GET
+        if analytics_query['date_from'] and analytics_query['date_to']:
+            ticker_obj = Company.objects.get(ticker=ticker)
+            date_from_obj = Historical.objects.filter(ticker=ticker_obj).get(date=analytics_query['date_from'])
+            date_to_obj = Historical.objects.filter(ticker=ticker_obj).get(date=analytics_query['date_to'])
+            difference = dict(open=round(date_to_obj.open - date_from_obj.open, 3),
+                              high=round(date_to_obj.high - date_from_obj.high, 3),
+                              low=round(date_to_obj.low - date_from_obj.low, 3),
+                              close=round(date_to_obj.close - date_from_obj.close, 3))
+
+            context = {'ticker': ticker,
+                       'date_from': date_from_obj,
+                       'date_to': date_to_obj,
+                       'difference': difference,
+                       }
+
+            return render(request, 'nasdaqstat/analytics.html', context)
+
+
+
+
+
+
+
